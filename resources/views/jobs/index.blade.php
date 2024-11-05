e4c6abbb5f4a2e6bd31de6e6fe733c1c29b85124
Ilya Usoltsev <ilya.usoltsev@zhinst.com>
2024-11-05 21:42:21 +0100

    Add create and index blade template

---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{$title}}</title>
+</head>
+
+<body>
+    <h1>{{$title}}</h1>
+    @if (!empty($jobs))
+    <ul>
+        @foreach($jobs as $job)
+            <li>{{$job}}</li>
+        @endforeach
+    </ul>
+    @else
+        <p>No jobs available</p>
+    @endif
+
+</body>
+
+</html>
+
+
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    $title = 'Available Jobs 2024';
+    $jobs = ['web developer', 'software engineer', 'network engineer'];
+    return view(
+        'jobs.index',
+        compact('title', 'jobs')
+    );
+})->name('jobs');
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs');
